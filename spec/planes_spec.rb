0503f548cbdbfb0667af1dec61f7eb4ed38fe358
tiffanie <yt.chia.2009@smu.edu.sg>
2014-03-07 12:50:45 +0000

    minor refactoring

--- spec/planes_spec.rb
+++ spec/planes_spec.rb
@@ -2,26 +2,24 @@ require './lib/planes'
 
 describe Planes do 
 
+let(:plane) {Planes.new}
+
 it "should not be flying when we initialize it" do
-	plane =Planes.new
 	expect(plane.flying?).to be_false
 end
 
 it "should be able to fly" do 
-	plane = Planes.new 
 	plane.fly
 	expect(plane.flying?).to be_true
 end
 
 it "should be able to land" do 
-	plane = Planes.new
 	plane.fly
 	plane.land
 	expect(plane.flying?).not_to be_true
 end	
 
 it "should know if it is flying" do
-	plane = Planes.new
 	plane.fly
 	expect(plane.flying?).to be_true
 	plane.land
@@ -29,7 +27,6 @@ it "should know if it is flying" do
 end	
 
 it "should be able to fly after taking off" do
-	plane = Planes.new
 	expect(plane.flying?).to be_false
 	plane.takeoff
 	expect(plane.flying?).to be_true
